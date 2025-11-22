41dfdbe2e340a97cf33891baa983a3bed1946cb6
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-11-22 13:43:32 +0530

    Update SageMaker Utilization Counter - Loader.user.js

--- SageMaker Utilization Counter - Loader.user.js
+++ SageMaker Utilization Counter - Loader.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name         SageMaker Utilization Counter - Loader (3-part)
-// @namespace    https://github.com/VigneshSankarP/Sagemaker_Tool
-// @version      1.0.0
-// @description  Loader script — requires core, footer and dashboard. Install only this script (Tampermonkey @require will load others).
-// @match        https://*.console.aws.amazon.com/*
+// @name         SM Test Loader (Dev Branch)
+// @namespace    sm-utilization
+// @version      1.0-test
+// @description  Loads core, footer, dashboard from dev folder for testing
+// @match        https://*.sagemaker.aws/*
 // @match        https://*.amazonaws.com/*
+// @match        https://*.console.aws.amazon.com/*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker%20Utilization%20Counter%20-%20Loader.user.js
-// @downloadURL  https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker%20Utilization%20Counter%20-%20Loader.user.js
-// @require https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/dev/sm-core-engine.user.js
-// @require https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/dev/sm-footer-ui.user.js
-// @require https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/dev/sm-dashboard.user.js
+// @require      https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/dev/sm-core.user.js
+// @require      https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/dev/sm-footer.user.js
+// @require      https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/dev/sm-dashboard.user.js
 // ==/UserScript==
 
-(function(){ 'use strict'; console.info("SM Loader active — core, footer, dashboard loaded via @require"); })();
+(function() {
+    console.log("%cSM TEST LOADER ACTIVE", "color: orange; font-weight: bold;");
+})();
