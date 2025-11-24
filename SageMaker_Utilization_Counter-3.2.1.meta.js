f4e61f53a412efea5db2aeb3510639a9c13de363
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-11-24 13:45:51 +0530

    Create SageMaker_Utilization_Counter-3.2.1.meta.js

---
+++ SageMaker_Utilization_Counter-3.2.1.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         SageMaker U+C+D - AI Enhanced (No Toast)
+// @namespace    http://tampermonkey.net/
+// @version      3.2.1-fixed-no-toast
+// @description  Ultra Premium Dashboard with AI Protection Engine - No Toast Notifications
+// @author       PVSANKAR
+// @homepageURL  https://github.com/VigneshSankarP/Sagemaker_Tool
+// @supportURL   https://github.com/VigneshSankarP/Sagemaker_Tool/issues
+// @updateURL    https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker_Utilization_Counter-3.2.1.meta.js
+// @downloadURL  https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker_Utilization_Counter-3.2.1.user.js
+// ==/UserScript==
