0865de63a9edea633053ac9d944ced2c1bdf2849
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-11-24 14:37:58 +0530

    Update and rename SageMaker_Utilization_Counter-3.2.1.meta.js to SageMaker_Utilization_Counter-1.7.meta.js

---
+++ SageMaker_Utilization_Counter-1.7.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Sagemaker Utilization Counter
+// @namespace    http://tampermonkey.net/
+// @version      3.2.1
+// @description  Ultra Premium Dashboard with AI Protection Engine - No Toast Notifications
+// @author       PVSANKAR
+// @homepageURL  https://github.com/VigneshSankarP/Sagemaker_Tool
+// @supportURL   https://github.com/VigneshSankarP/Sagemaker_Tool/issues
+// @updateURL    https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker%20Utilization%20Counter-1.7.meta.js
+// @downloadURL  https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/SageMaker%20Utilization%20Counter-1.7.user.js
+// ==/UserScript==
