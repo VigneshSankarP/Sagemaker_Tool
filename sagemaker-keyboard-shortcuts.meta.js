99d08d78fa0e62c75b0aaecf5ceb8b5c52b1bdcc
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-11-26 22:49:26 +0530

    Create SageMaker Keyboard Shortcuts UserScript
    This script adds keyboard shortcuts for options 11-14 in SageMaker labeling tasks.

---
+++ sagemaker-keyboard-shortcuts.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         SageMaker Keyboard Shortcuts
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Add Q W E R keyboard shortcuts for options 11-14 in SageMaker labeling tasks
+// @author       PVSANKAR
+// @match        *://*.sagemaker.aws/*
+// @match        https://dcjt2af5rw.labeling.us-west-2.sagemaker.aws/*
+// @match        https://*.amazonaws.com/*
+// @grant        none
+// @run-at       document-idle
+// @homepageURL  https://github.com/VigneshSankarP/Sagemaker_Tool
+// @supportURL   https://github.com/VigneshSankarP/Sagemaker_Tool/issues
+// @updateURL    https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/sagemaker-keyboard-shortcuts.meta.js
+// @downloadURL  https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/sagemaker-keyboard-shortcuts.user.js
+// @icon         https://raw.githubusercontent.com/VigneshSankarP/Sagemaker_Tool/main/icon.png
+// ==/UserScript==
