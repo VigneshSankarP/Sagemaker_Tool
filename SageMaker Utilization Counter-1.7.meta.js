750d80be75e8d10aaa8050200dd3374dea68a08e
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-12-29 21:09:12 +0530

    Update version number to 8 in meta.js

--- SageMaker Utilization Counter-1.7.meta.js
+++ SageMaker Utilization Counter-1.7.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sagemaker Utilization Counter
 // @namespace    https://github.com/VigneshSankarP
-// @version      7
+// @version      8
 // @description  Dashboard - Optimized for 8+ Hour Sessions
 // @author       PVSANKAR
 // @homepageURL  https://github.com/VigneshSankarP/Sagemaker_Tool
