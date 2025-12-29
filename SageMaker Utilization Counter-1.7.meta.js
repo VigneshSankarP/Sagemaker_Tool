750905cd77a58b601dcb80f51a0a619606cc29e8
Vignesh Sankar <142923733+VigneshSankarP@users.noreply.github.com>
2025-12-29 11:37:25 +0530

    Bump version from 5 to 6 in meta file

--- SageMaker Utilization Counter-1.7.meta.js
+++ SageMaker Utilization Counter-1.7.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sagemaker Utilization Counter
 // @namespace    https://github.com/VigneshSankarP
-// @version      5
+// @version      6
 // @description  Dashboard - Optimized for 8+ Hour Sessions
 // @author       PVSANKAR
 // @homepageURL  https://github.com/VigneshSankarP/Sagemaker_Tool
